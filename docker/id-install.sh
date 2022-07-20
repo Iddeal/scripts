008fc2bbbf20a7838648459598120bb41fb58f48
Michael Minton <michael@minton.io>
2022-07-19 21:09:23 -0400

    Delete id-install.sh

--- docker/id-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install the up.sh and down.sh scripts into the specified folder
-(cd $1 && cp ~/.iddeal-scripts/up.sh . && chmod a+x up.sh)
-(cd $1 && cp ~/.iddeal-scripts/down.sh . && chmod a+x down.sh)
-
-echo "Scripts installed to $1."
