d240792487b96e4a667106cfa01589f3844a2c1a
Michael Minton <michael@minton.io>
2022-07-19 20:00:20 -0400

    Update id-new.sh

--- docker/id-new.sh
+++ docker/id-new.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Create a new container to run mix phx.new inside
+echo "Creating app using Phoenix container..."
 docker run -v $(pwd):/app --rm --name="idx-gen-new-phx-temp" iddmichael/phx1.6.10-elixir1.13.4-erlang-25.0.2-dev:2 yes | mix phx.new $1 && docker stop idx-gen-new-phx-temp
+
+# Install the up.sh and down.sh scripts into the specified folder
+(cd $1 && cp ~/.iddeal-scripts/up.sh . && chmod a+x up.sh)
+(cd $1 && cp ~/.iddeal-scripts/down.sh . && chmod a+x down.sh)
+
+echo "Scripts installed to $1."
