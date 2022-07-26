228c3802591e665ed1a2e8001d1a6c283c51342a
Michael Minton <michael@minton.io>
2022-07-26 08:50:27 -0400

    Update id-ex-new.sh

--- docker/id-ex-new.sh
+++ docker/id-ex-new.sh
@@ -2,7 +2,7 @@
 
 # Create a new container to run mix phx.new inside
 echo "Creating app using Phoenix container..."
-docker run -v $(pwd):/app --rm --name="idx-gen-new-phx-temp" iddmichael/phx1.6.10-elixir1.13.4-erlang-25.0.2-dev:2 yes | mix phx.new $1
+docker run -v $(pwd):/app --rm --name="idx-gen-new-phx-temp" iddmichael/phx1.6.10-elixir1.13.4-erlang-25.0.2-dev:2 yes | mix phx.new "$@"
 docker stop idx-gen-new-phx-temp
 
 echo -e "\033[0;32m Generated $1.\033[0m"
